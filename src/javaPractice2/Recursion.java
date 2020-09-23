261cf1ed0877be93354942a5116c49a433914823
Jean-Yves K <jkwibuka@yahoo.co.nz>
2020-09-23 18:57:49 +1200

    Recursion - Factorials

---
+++ src/javaPractice2/Recursion.java
@@ -0,0 +1,29 @@
+package javaPractice2;
+
+import java.util.Scanner;
+
+
+public class Recursion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Factorials are made by recursively multiplying. E.g: 5! = 5 x 4 x 3 x 2 x 1 = 120
+		
+		//We can achieve this by using a method that calls on itself
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Please enter a number you'd like to factorise");
+		int input = userInput.nextInt();
+		System.out.println("Okay, so: "+input+"! = "+factorial(input));
+		main(args);
+	}
+
+	private static int factorial(int x) {
+		if(x == 1){
+			return 1;
+		}else {
+			return x * factorial(x - 1);
+		}
+	}
+
+}
