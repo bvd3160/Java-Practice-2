d0d9c61fd4e06ac3332fa4d8f103dcd13d038a82
Jean-Yves K <jkwibuka@yahoo.co.nz>
2020-10-12 13:06:48 +1300

    Some game logic added to Tic Tac Toe Game - comments added for clarity and train of thought

--- src/ticTacToe/TicTacToe.java
+++ src/ticTacToe/TicTacToe.java
@@ -1,5 +1,9 @@
 package ticTacToe;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 import javax.sound.midi.VoiceStatus;
 
 public class TicTacToe {
@@ -10,16 +14,75 @@ public class TicTacToe {
             {' ', '|', ' ', '|', ' '},
             {'-', '+', '-', '+', '-'},
             {' ', '|', ' ', '|', ' '}};
+	
+	private static ArrayList<String> players = new ArrayList<String>();
 
 	public static void main(String[] args) {
 		//Building the Tic-Tac-Toe board
-		refereshBoard(playingField);
+		loadGameboard(playingField);
 		
-		System.out.println("First move play");
-		playMove(playingField, 5, "player");
+		//set players of the game
+		players.add("cpu");
+		players.add("player");
+		
+		playGame();
+		
 	}
+	
+	
+	/**
+	 * playGame(): all game logic resides here
+	 */
+	public static void playGame() {
+		//get a random player to start the game
+		String player = players.get(new Random().nextInt(2));
+		//need to run game continuously until a winner is found
+		while(true) {
+			if(player.equals("player") && !isGameTied(playingField)) {
+				Scanner userInput = new Scanner(System.in); 
+				System.out.println("Please enter a position " + player);
+				int pPosition = userInput.nextInt();
+				//check that position isn't taken and if so, get player to select another//
+				
+				placePiece(playingField, pPosition, "player");
+				//Player should now be set to cpu next for next time
+				player = "cpu";
+				loadGameboard(playingField);
+				//checkWinner - break out of loop if Player won
+				//checkWinner();
+				
+				
+			}else if(player.equals("cpu") && !isGameTied(playingField)){
+				int cPosition = new Random().nextInt(9)+1;
+				//need to check that position isn't taken before placing piece or generate another//
+				
+				System.out.println(player + " played: " + cPosition);
+				placePiece(playingField, cPosition, player);
+				//Player should now be set to player next for next time
+				player = "player";
+				loadGameboard(playingField);
+				//checkWinner - break out of loop if CPU won
+				//checkWinner();
+				
+			}else
+				System.out.println("No Winner Found!");
+				//break;
+		}
+	}
+	
+	private static boolean isGameTied(char [][] board) {
+		boolean isTied = false;
+		
+		return isTied;
+	}
+	
+	
 
-	private static void refereshBoard(char [][] board) {
+	/**
+	 * loadGameboard(): redraws the Tic-Tac-Toe board on the screen
+	 * @param board: the board being drawn
+	 */
+	private static void loadGameboard(char [][] board) {
 		System.out.println("===== TicTacToe =====");
 		for (char[] cs : board) {
 			System.out.print(cs);
@@ -29,7 +92,14 @@ public class TicTacToe {
 				+ "----------------------------------------- \n");
 	}
 	
-	public static void playMove(char[][] playingField, int pos, String player) {
+		
+	/**
+	 * placePiece(): places a piece on the board and shows us the board again
+	 * @param playingField: the Tic-Tac-Toe board
+	 * @param pos: the position of move being played
+	 * @param player: which player's turn it is
+	 */
+	public static void placePiece(char[][] playingField, int pos, String player) {
 		int position = 1;
 		char playerSymbol = 'Y';
 		
@@ -66,8 +136,8 @@ public class TicTacToe {
 			break;
 		}
 		
-		refereshBoard(playingField);
-		
 	}
+	
+	
 
 }
