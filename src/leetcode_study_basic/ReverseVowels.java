06489e917d8e12384405091303ee62bdfa78bc64
Jean-Yves K <jkwibuka@yahoo.co.nz>
2020-10-22 12:39:54 +1300

    Reverse Vowels Challenge

--- src/leetcode_study_basic/ReverseVowels.java
+++ src/leetcode_study_basic/ReverseVowels.java
@@ -4,12 +4,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ReverseVowels {
+	
+	/*
+	 * Write a function that takes a string as input and reverse only the vowels of
+	 * a string.
+	 * 
+	 * Example 1:
+	 * 
+	 * Input: "hello" Output: "holle" Example 2:
+	 * 
+	 * Input: "leetcode" Output: "leotcede" Note: The vowels does not include the
+	 * letter "y".
+	 */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(reverseVowels("leetcode challenge"));
 	}
 	
+	/**
+	 * @param s: string to be converted
+	 * @return
+	 */
 	public static String reverseVowels(String s) {
 		String result = "";
 		ArrayList<Character> vowelsTemp = new ArrayList<>();
